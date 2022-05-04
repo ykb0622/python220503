19f93f39b431fa9dab793f8db56912ec4496fa3f
ykb0622 <ykb0622@naver.com>
2022-05-04 16:35:46 +0900

    파일 완료

--- Demofile2.py
+++ Demofile2.py
@@ -1,39 +1,35 @@
-# Demofile2.py
+# DemoFile2.py 
 
-# 파일에 쓰기(r: raw string notation)
-# 로우 푸드(가공하지 않은 음식) : 통밀빵, 현미 ...
-f = open(r"c:\work\test.txt","wt", encoding="utf-8")
+#파일에 쓰기(r: raw string notation)
+#로우 푸드(가공하지 않은 음식): 통밀빵, 현미... 
+f = open(r"c:\work\test.txt", "wt", encoding="utf-8")
 f.write("첫줄에\n두번째는\n마지막\n")
-f.close()
+f.close() 
 
-# 파일읽기
-f = open(r"c:\work\test.txt","rt", encoding="utf-8")
+#파일 읽기
+f = open(r"c:\work\test.txt", "rt", encoding="utf-8")
 result = f.read()
 print(result)
-
 #처음으로 이동
 print(f.tell())
 f.seek(0)
-
-# 약간의 보정
-print(f.readline())
-print(f.readline())
-print(f.readline())
-
+#약간의 보정
+print(f.readline(), end="")
+print(f.readline(), end="")
+print(f.readline(), end="")
 #처음으로 이동
-print(f.tell())
-lst=f.readline()
+f.seek(0)
+lst = f.readlines() 
 print(lst)
 for item in lst:
     print(item, end="")
 
-f.close()
+f.close() 
 
-# with절을 같이 사용(close()메서드를 자동 호출)
+#with절을 같이 사용(close()메서드를 자동 호출)
+print("---with키워드---")
 with open("c:\\work\\test.txt", encoding="utf-8") as f:
     result = f.readline()
     while result != '':
         print(result, end="")
-        result = f.readline()
-
-
+        result = f.readline() 
