8780610eea6be6400a1e8dd918cd698ac4c9dce0
ykb0622 <ykb0622@naver.com>
2022-05-04 10:37:29 +0900

    클래스연습

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스 정의 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,16 +7,23 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
 
+#자식 클래스 정의 
 class Student(Person):
+    #덮어쓰기(재정의)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모의 생성자 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
-
+    #덮어쓰기(override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(
+            self.subject, self.studentID))
+            
+#인스턴스 생성
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
+s = Student("이순신", "010-111-1234", "빅데이터", "201122")
 p.printInfo()
 s.printInfo()
-
