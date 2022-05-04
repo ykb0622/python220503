c937f03ae0d46e4b99bda9adfa8d21d315af701b
ykb0622 <ykb0622@naver.com>
2022-05-04 10:07:14 +0900

    뱅크어카운트

--- BankAccount.py
+++ BankAccount.py
@@ -1,23 +1,23 @@
 # BankAccount.py
-
+#보안을 강화하기 위해 변수를 내부에 숨기기(이름 변경)
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #클래스 내부에 멤버변수를 숨김(이름 변경)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
     def __str__(self):
-        return "{0}, {1}, {2}".format(self.id, 
-        self.name, self.balance)
+        return "{0}, {1}, {2}".format(self.__id, 
+        self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
 print(account1)
-
-
-
+#외부에서는 _클래스명__변수명
+print(account1._BankAccount__balance)
