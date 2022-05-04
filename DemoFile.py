e74055ddbd79b0da947ded08088fac97c4c09e9e
ykb0622 <ykb0622@naver.com>
2022-05-04 15:36:46 +0900

    서식지정

--- DemoFile.py
+++ DemoFile.py
@@ -7,6 +7,31 @@ import sys
 
 print("welcome to", "python", sep="~", end="!", file=sys.stderr)
 
+
 f = open("c:\\work\\demo.txt","wt")
 print("file write", file=f)
 f.close()
+
+#구구단 출력
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x))
+
+
+print("---약간 보정---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).rjust(3))
+
+print("---약간 보정---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).zfill(3))
+
+# 서식문자를 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:c}".format(65))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
+# 화폐 형태로 출력
+print("{0:,}".format(150000000))
+
