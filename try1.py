271bf450415015e115bc7c35ca0e21bb54203142
ykb0622 <ykb0622@naver.com>
2022-05-04 14:45:20 +0900

    에러처리

--- try1.py
+++ try1.py
@@ -3,9 +3,19 @@
 def divide(a,b):
     return a/b
 
-# 함수 호출
-result = divide(5, 0)
-print("결과"{0}.format(result))
+#에러처리
+try:
+    # 함수 호출
+    result = divide(5, 0)
+    print("결과:{0}".format(result))
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다")
+except TypeError:
+    print("숫자여야 합니다")
+else:
+    print("결과:{0}".format(result))
+finally:
+    print("한번 더 체크")
 
 print("---전체 코드 실행 종료---")
 
