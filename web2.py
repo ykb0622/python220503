2b50586f778ecf5bf7c13881282adad094b9484e
ykb0622 <ykb0622@naver.com>
2022-05-06 13:41:17 +0900

    웹툰 크롤링

---
+++ web2.py
@@ -0,0 +1,5 @@
+# web2.py
+
+<td class="title">
+                <a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+                        </td>
