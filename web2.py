0dbe2cd971be9c1b991285372b17f5d649ab5349
ykb0622 <ykb0622@naver.com>
2022-05-06 14:04:48 +0900

    웹툰 크롤링

--- web2.py
+++ web2.py
@@ -1,5 +1,21 @@
 # web2.py
+#웹서버에 페이지 실행 요청
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
 
-<td class="title">
-                <a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
-                        </td>
+# <td class="title">
+#                 <a href="/webtoon/detail?">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+#                         </td>
+
+
+data = urllib.request.urlopen(
+    "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
+
+soup = BeautifulSoup(data, "html.parser")
+
+cartoons = soup.find_all("td", class_="title")
+title = cartoons[0].find("a").text
+link = cartoons[0].find("a")["href"]
+print(title)
+print(link)
