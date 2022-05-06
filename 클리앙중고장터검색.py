5aefabcf99af0a6bf6459bdfc059668ec94968a0
ykb0622 <ykb0622@naver.com>
2022-05-06 15:03:15 +0900

    클리앙

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -1,32 +1,36 @@
 # coding:utf-8
 from bs4 import BeautifulSoup
 import urllib.request
+#정규표현식 사용 
 import re 
 
+#웹봇(웹로봇)
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
-        req = urllib.request.Request(data)
         data = urllib.request.urlopen(req).read()
+        #한글이 깨지는 경우 
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+
+        # <span class="subject_fixed" data-role="list-title-text">
+        # 	스타벅스 별쿠폰 1매
+        # </span>
+        #특정 태그의 속성들(attrs)
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
-                        if (re.search('아이폰', title)):
+                        title = item.text 
+                        #print(title.strip())
+                        if (re.search('애플', title)):
                                 print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
                 except:
                         pass
-        
